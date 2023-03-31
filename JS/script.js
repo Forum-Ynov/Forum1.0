7d8e07da4745e8843b4a8f004dfdae0bcb3d9b23
Theo-06130 <theo.cerkownik@ynov.com>
2023-03-31 09:36:34 +0200

    logo

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
 const toggleMenu = () => document.body.classList.toggle("open");
 
 const body = document.querySelector('body');
+const logo_dark = document.getElementById('logo_dark')
+const logo_light = document.getElementById('logo_light')
+
+console.log(logo_dark);
+console.log(logo_light);
 // const heart = document.getElementById('like');
 // const heart_red = document.getElementById('like_red')
 // console.log(heart);
@@ -8,10 +13,15 @@ function switch_theme() {
     if (test.checked) {
         console.log("dark");
         body.setAttribute('data-theme', 'dark');
+        logo_light.style.opacity = 0;
+        logo_dark.style.opacity = 1;
     }
     else {
         console.log("light");
         body.setAttribute('data-theme', 'light');
+        logo_light.style.opacity = 1;
+        logo_dark.style.opacity = 0;
+
     }
 }
 
