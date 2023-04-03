74a45e2f8159f4126925294fc77cd2707530e3ac
Theo-06130 <theo.cerkownik@ynov.com>
2023-04-03 08:55:09 +0200

    del comment

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,7 @@ const logo_light = document.getElementById('logo_light')
 
 console.log(logo_dark);
 console.log(logo_light);
-// const heart = document.getElementById('like');
-// const heart_red = document.getElementById('like_red')
-// console.log(heart);
+
 function switch_theme() {
     if (test.checked) {
         console.log("dark");
@@ -25,13 +23,4 @@ function switch_theme() {
     }
 }
 
-// heart.addEventListener("click", function (e) {
-//     console.log(e);
-//     heart.style.display = 'none';
-//     heart_red.style.opacity = 1
-// });
-
-// heart_red.addEventListener("click", function (e) {
-//     heart_red.style.opacity = '1'
-// })
 
