8a9bcae94e45ce4fc4208384eb561f2dab6c0ebe
Theo-06130 <theo.cerkownik@ynov.com>
2023-04-03 11:42:44 +0200

    popup3

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
+
+import { User } from "./user_class.js"
+
 const toggleMenu = () => document.body.classList.toggle("open");
+
+const swmode = document.getElementById("swmode")
 
 const body = document.querySelector('body');
 const logo_dark = document.getElementById('logo_dark')
@@ -12,14 +17,51 @@ function switch_theme() {
     if (test.checked) {
         console.log("dark");
         body.setAttribute('data-theme', 'dark');
-        logo_light.style.opacity = 0;
-        logo_dark.style.opacity = 1;
+
+        const localUser = localStorage.getItem("loged_user")?.toString()
+        let storageUser = new User("", "", "", "", "", "");
+
+        if (localUser) {
+            storageUser = JSON.parse(localUser)
+            console.log(storageUser)
+
+            const r = await fetch(`http://localhost:8000/usertheme/${storageUser.id_user}`, {
+                method: 'PATCH',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify({ theme: "dark" })
+            })
+            storageUser.theme = "dark"
+            localStorage.setItem("loged_user", JSON.stringify(storageUser))
+
+        }
+
     }
     else {
         console.log("light");
         body.setAttribute('data-theme', 'light');
-        logo_light.style.opacity = 1;
-        logo_dark.style.opacity = 0;
+
+        const localUser = localStorage.getItem("loged_user")?.toString()
+        let storageUser = new User("", "", "", "", "", "");
+
+        if (localUser) {
+            storageUser = JSON.parse(localUser)
+            console.log(storageUser)
+
+            const r = await fetch(`http://localhost:8000/usertheme/${storageUser.id_user}`, {
+                method: 'PATCH',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify({ theme: "light" })
+            })
+            storageUser.theme = "light"
+            localStorage.setItem("loged_user", JSON.stringify(storageUser))
+
+        }
 
     }
 }
