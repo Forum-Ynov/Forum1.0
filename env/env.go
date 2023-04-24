acf4ca553c3dca48223364a163ff53ca6a9e1f6f
Sqwado <mateo.luque@ynov.com>
2023-04-24 09:23:24 +0200

    db switch

--- env/env.go
+++ env/env.go
@@ -5,9 +5,11 @@ import "os"
 // Variables d'environnement
 var (
 	Api_port string
-	Password string
-	User     string
+
+	Host     string
 	Port     string
+	User     string
+	Password string
 	Database string
 	Sql_db   string
 )
@@ -15,9 +17,12 @@ var (
 // SetEnv récupère les variables d'environnement
 func SetEnv() {
 	Api_port = os.Getenv("API_PORT")
-	Password = os.Getenv("DB_PASSWORD")
-	User = os.Getenv("DB_USER")
+
+	Host = os.Getenv("DB_HOST")
 	Port = os.Getenv("DB_PORT")
+	User = os.Getenv("DB_USER")
+	Password = os.Getenv("DB_PASSWORD")
 	Database = os.Getenv("DB_DATABASE")
-	Sql_db = User + ":" + Password + "@tcp(" + Port + ")/" + Database + "?parseTime=true"
+
+	Sql_db = User + ":" + Password + "@tcp(" + Host + ":" + Port + ")/" + Database + "?parseTime=true"
 }
